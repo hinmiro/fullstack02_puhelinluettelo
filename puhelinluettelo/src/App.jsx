b054db15806eb91e363e938a95c52e6acc885f44
mirohi <m_hintikka@hotmail.com>
2024-05-21 22:53:43 +0300

    Add 2.6 step 1

--- puhelinluettelo/src/App.jsx
+++ puhelinluettelo/src/App.jsx
@@ -1,22 +1,53 @@
 import { useState } from "react";
 
+const Person = ({ person }) => {
+  return (
+    <tr>
+      <td>{person.name}</td>
+    </tr>
+  );
+};
+
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const handleNewName = (event) => {
+    event.preventDefault();
+    setNewName(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setPersons(persons.concat({ name: newName }));
+    setNewName("");
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNewName} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      ...
+      <table>
+        <thead>
+          <tr>
+            <th>
+              <h2>Numbers</h2>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {persons.map((person, i) => (
+            <Person key={i} person={person} />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
