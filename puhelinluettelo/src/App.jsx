c95f7e99574dd7a8939c705ec0c9321221fd76a7
mirohi <m_hintikka@hotmail.com>
2024-05-26 21:25:19 +0300

    Add assignments 2.11

--- puhelinluettelo/src/App.jsx
+++ puhelinluettelo/src/App.jsx
@@ -1,13 +1,15 @@
 import PersonForm from "./components/PersonForm.jsx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Tony Stark", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  });
   return (
     <div>
       <h2>Phonebook</h2>
