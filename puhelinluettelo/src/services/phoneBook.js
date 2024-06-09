045cc39ab3bfbd2fa4b76502ad8885bbac892225
mirohi <m_hintikka@hotmail.com>
2024-06-09 18:01:31 +0300

    Prepare app for intergalactic journey

--- puhelinluettelo/src/services/phoneBook.js
+++ puhelinluettelo/src/services/phoneBook.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001/api/persons";
+const baseUrl = "https://fullstack03.onrender.com/api/persons";
 
 const getAll = () => {
   try {
